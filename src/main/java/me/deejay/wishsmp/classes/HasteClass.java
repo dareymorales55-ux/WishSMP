75c0832a5f7641ca330cf78012d864f0d7366729
dareymorales55-ux <dareymorales55@gmail.com>
2026-03-01 14:38:11 -0800

    Create HasteClass.java

---
+++ src/main/java/me/deejay/wishsmp/classes/HasteClass.java
@@ -0,0 +1,10 @@
+package me.deejay.wishsmp.classes;
+
+import org.bukkit.entity.Player;
+
+public class HasteClass {
+
+    public static void applyEffects(Player player) {
+        // Placeholder — nothing yet
+    }
+}
