1fcb7e4b111a2d0447b3c8d8c9915498453433b4
dareymorales55-ux <dareymorales55@gmail.com>
2026-03-01 14:36:41 -0800

    Create StrengthClass.java

---
+++ src/main/java/me/deejay/wishsmp/classes/StrengthClass.java
@@ -0,0 +1,10 @@
+package me.deejay.wishsmp.classes;
+
+import org.bukkit.entity.Player;
+
+public class StrengthClass {
+
+    public static void applyEffects(Player player) {
+        // Placeholder — nothing yet
+    }
+}
