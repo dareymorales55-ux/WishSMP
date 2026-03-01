07e2e2ed530690db72a40a8d2ac0509398e31379
dareymorales55-ux <dareymorales55@gmail.com>
2026-03-01 14:37:42 -0800

    Create HealthClass.java

---
+++ src/main/java/me/deejay/wishsmp/classes/HealthClass.java
@@ -0,0 +1,10 @@
+package me.deejay.wishsmp.classes;
+
+import org.bukkit.entity.Player;
+
+public class HealthClass {
+
+    public static void applyEffects(Player player) {
+        // Placeholder — nothing yet
+    }
+}
