af5745ed1359d2cbe111400c5e9f1e850b5c9381
dareymorales55-ux <dareymorales55@gmail.com>
2026-09-01 01:17:33 -0700

    Add SpiritType enum with display names and methods

--- src/main/java/com/vessel/smp/data/SpiritType.java
+++ src/main/java/com/vessel/smp/data/SpiritType.java
@@ -1,1 +1,33 @@
 
+package com.vessel.smp.data;
+
+public enum SpiritType {
+
+    WARDEN("Warden"),
+    DOLPHIN("Dolphin"),
+    VILLAGER("Villager"),
+    TURTLE("Turtle");
+
+    private final String displayName;
+
+    SpiritType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public static SpiritType random() {
+        SpiritType[] values = values();
+        return values[(int) (Math.random() * values.length)];
+    }
+
+    public static SpiritType randomExcluding(SpiritType exclude) {
+        SpiritType result;
+        do {
+            result = random();
+        } while (result == exclude);
+        return result;
+    }
+}
